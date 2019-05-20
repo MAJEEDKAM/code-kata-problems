427be7a8778ec8629c2eb4ab1abe324ac97cbdea
MAJEEDKAM <46222521+MAJEEDKAM@users.noreply.github.com>
2019-05-21 00:20:57 +0530

    Update que3.py

--- que3.py
+++ que3.py
@@ -3,7 +3,7 @@ char =(input())
 vowels=['a','e','i','o','u','A','E','I','O','U']
 
 if char in vowels:
-        print("Vowels")
+        print("Vowel")
 
 elif char.isalpha():
         print("Consonant")
