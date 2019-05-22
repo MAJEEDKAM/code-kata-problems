af54a44d79946f3f087a83e1864bff07df5f8d7e
MAJEEDKAM <46222521+MAJEEDKAM@users.noreply.github.com>
2019-05-22 23:29:07 +0530

    Update que36.py

--- que36.py
+++ que36.py
@@ -2,6 +2,6 @@ import re
 paragraph=input()
 count=0
 for i in paragraph:
-    if re.match("^[.,@_!#$%^&*()<>?/\|}{~:]*$",i):
+    if re.match("{}[]!@#$%^&*()_:<>?;'\?~`.,",i):
                    count+=1
 print(count)
